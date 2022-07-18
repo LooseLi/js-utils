45d2938e25eebe6dc20b1221c4d2ab28dcabfbed
LooseLi <1329307562@qq.com>
2022-07-18 10:41:41 +0800

    :tada: init

--- utils/is.js
+++ utils/is.js
@@ -5,3 +5,7 @@ const UA = inBrowser && window.navigator.userAgent.toLowerCase();
 export const isEdge = UA && UA.indexOf('edge/') > 0;
 
 export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge;
+
+export const isAndroid = UA && UA.indexOf('android') > 0;
+
+export const isIOS = UA && /iphone|ipad|ipod|ios/.test(UA);
