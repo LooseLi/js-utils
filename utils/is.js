d1da474f131b4c4e4c5f831f3a6fbf1625861c97
LooseLi <1329307562@qq.com>
2022-07-18 11:07:51 +0800

    :pencil: judge type

--- utils/is.js
+++ utils/is.js
@@ -9,3 +9,27 @@ export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge;
 export const isAndroid = UA && UA.indexOf('android') > 0;
 
 export const isIOS = UA && /iphone|ipad|ipod|ios/.test(UA);
+
+export const isString = value => {
+  return Object.prototype.toString.call(value) === '[object String]';
+};
+
+export const isNumber = value => {
+  return Object.prototype.toString.call(value) === '[object Number]';
+};
+
+export const isBoolean = value => {
+  return Object.prototype.toString.call(value) === '[object Boolean]';
+};
+
+export const isObject = object => {
+  return object !== null && Object.prototype.toString.call(object) === '[object Object]';
+};
+
+export const isDate = date => {
+  return Object.prototype.toString.call(date) === '[object Date]';
+};
+
+export const isArray = arr => {
+  return arr && Array.isArray(arr);
+};
