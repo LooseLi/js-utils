8ee6f2f334aa3183f6d05c90a4e47763f3599586
LooseLi <1329307562@qq.com>
2022-07-21 16:37:50 +0800

    :pencil: 简单数组去重

--- utils/base.js
+++ utils/base.js
@@ -2,3 +2,7 @@ export function randomInt(min, max) {
   const c = max - min + 1;
   return Math.floor(Math.random() * c + min);
 }
+
+export function arrayRepeat(arr) {
+  return [...new Set(arr)];
+}
