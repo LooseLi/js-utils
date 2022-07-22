2571482e88640a7bf0a672f75bf3119d5262858c
LooseLi <1329307562@qq.com>
2022-07-22 15:45:14 +0800

    :pencil: dom相关

--- utils/dom.js
+++ utils/dom.js
@@ -7,6 +7,7 @@ export function hasClass(el, cls) {
   return el.classList.contains(cls);
 }
 
+// add class
 export function addClass(el, cls) {
   if (!el) return;
   const classes = (cls || '').split(' ');
@@ -15,4 +16,20 @@ export function addClass(el, cls) {
     if (!clsName || hasClass(el, cls)) continue;
     el.classList.add(clsName);
   }
+}
+
+// remove class
+export function removeClass(el, cls) {
+  if (!el || !cls) return;
+  const classes = (cls || '').split(' ');
+  for (let i = 0; i < classes.length; i++) {
+    const clsName = classes[i];
+    if (!clsName) continue;
+    el.classList.remove(clsName);
+  }
+}
+
+export function getBoundingClientRect(el) {
+  if (!el || !el.getBoundingClientRect) return;
+  console.log(el.getBoundingClientRect());
 }
