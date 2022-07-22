257d3e49472e0a1d1fb7d0bc08b7e8523fb10826
LooseLi <1329307562@qq.com>
2022-07-22 15:31:47 +0800

    :pencil: dom相关

--- utils/dom.js
+++ utils/dom.js
@@ -1,5 +1,18 @@
-export function getElement(el) {
-  return el && document.querySelector(el);
+export function getElement(selector) {
+  return selector && document.querySelector(selector);
 }
 
-export function addClass() {}
+export function hasClass(el, cls) {
+  if (!el || !cls || !el.classList.length) return false;
+  return el.classList.contains(cls);
+}
+
+export function addClass(el, cls) {
+  if (!el) return;
+  const classes = (cls || '').split(' ');
+  for (let i = 0; i < classes.length; i++) {
+    const clsName = classes[i];
+    if (!clsName || hasClass(el, cls)) continue;
+    el.classList.add(clsName);
+  }
+}
