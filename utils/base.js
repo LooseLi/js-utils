d9fdca5843138a9eb24ea83f9f254b76d2fac62d
LooseLi <1329307562@qq.com>
2022-08-11 16:39:13 +0800

    :pencil: localStorage

--- utils/base.js
+++ utils/base.js
@@ -74,3 +74,14 @@ export function subText(str, length) {
 
 // 返回当前url
 export const currentURL = () => window.location.href;
+
+// set localStorage
+export const localStorageSet = (key, value) => {
+  if (typeof value === 'object') value = JSON.stringify(value);
+  localStorage.setItem(key, value);
+};
+
+// get localStorage
+export const localStorageGet = key => {
+  return localStorage.getItem(key);
+};
