bc6cba5c00de09e9961a767d1177c22448863087
LooseLi <1329307562@qq.com>
2022-08-15 16:59:11 +0800

    :pencil: base64ToFile | base64ToBlob

--- utils/base.js
+++ utils/base.js
@@ -71,3 +71,40 @@ export function subText(str, length) {
     return str;
   }
 }
+
+/**
+ * base64ToFile
+ * @param {*} base64
+ * @param {*} filename
+ * @returns
+ */
+export const base64ToFile = (base64, filename) => {
+  const arr = base64.split(',');
+  const mime = arr[0].match(/:(.*?);/)[1];
+  // 图片后缀
+  const suffix = mime.split('/')[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], `${filename}.${suffix}`, {type: mime});
+};
+
+/**
+ * base64ToBlob
+ * @param {*} base64
+ * @returns
+ */
+export const base64ToBlob = base64 => {
+  let arr = base64.split(','),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], {type: mime});
+};
