1e2c0e7925a5d48ae787cd9746b4b5703c7b2b48
LooseLi <1329307562@qq.com>
2022-08-16 11:19:08 +0800

    :pencil: es6

--- utils/dom.js
+++ utils/dom.js
@@ -1,14 +1,14 @@
-export function getElement(selector) {
+export const getElement = selector => {
   return selector && document.querySelector(selector);
-}
+};
 
-export function hasClass(el, cls) {
+export const hasClass = (el, cls) => {
   if (!el || !cls || !el.classList.length) return false;
   return el.classList.contains(cls);
-}
+};
 
 // add class
-export function addClass(el, cls) {
+export const addClass = (el, cls) => {
   if (!el) return;
   const classes = (cls || '').split(' ');
   for (let i = 0; i < classes.length; i++) {
@@ -16,10 +16,10 @@ export function addClass(el, cls) {
     if (!clsName || hasClass(el, cls)) continue;
     el.classList.add(clsName);
   }
-}
+};
 
 // remove class
-export function removeClass(el, cls) {
+export const removeClass = (el, cls) => {
   if (!el || !cls) return;
   const classes = (cls || '').split(' ');
   for (let i = 0; i < classes.length; i++) {
@@ -27,10 +27,10 @@ export function removeClass(el, cls) {
     if (!clsName) continue;
     el.classList.remove(clsName);
   }
-}
+};
 
 // 元素的大小及其相对于视口的位置
-export function getBoundingClientRect(el) {
+export const getBoundingClientRect = el => {
   if (!el || !el.getBoundingClientRect) return;
   return el.getBoundingClientRect();
-}
+};
